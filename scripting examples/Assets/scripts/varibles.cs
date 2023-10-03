03760eea0bf6cc67f59fd95520e5607bb5b55ab9
Morbius <Morghandenham097@riversideunified.org>
2023-10-03 14:22:21 -0700

    nuh uh

---
+++ scripting examples/Assets/scripts/varibles.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class varibles : MonoBehaviour
+{
+    public int Score = 0//declare whole number
+    public float ItemAmount = 19.95f; //declare a decimal number
+    public string PlayerName = "The_Rizzler";//declare a text
+    public bool IsPlayerActive = true;// declare a true/false
+
+    // Start is called before the first frame update
+    private int _collectibleAmound = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
