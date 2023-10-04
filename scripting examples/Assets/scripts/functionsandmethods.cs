bc7835f17f5df005885e4a34d24e43dcb307a16d
Morbius <Morghandenham097@riversideunified.org>
2023-10-04 13:31:22 -0700

    Update functionsandmethods.cs

--- scripting examples/Assets/scripts/functionsandmethods.cs
+++ scripting examples/Assets/scripts/functionsandmethods.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class functionsandmethods : MonoBehaviour
 {
+    private int _playerHealthAmount = 100;
+    public int Score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,26 @@ public class functionsandmethods : MonoBehaviour
     {
         
     }
+
+    //method examples
+
+    //this method is accesable from other files becuase its public
+    public void UpdatePlayerHealth(int amount)
+    {
+        _playerHealthAmount -= amount;
+    }
+
+
+    //this method is not assecable from other files
+    private void OnBecameInvisible()
+    {
+        Score++; //score = score + 1
+    }
+
+    private void MovePlayer() 
+    {
+        transform.Translate(Vector3.forward);
+    }
 }
+
+  
